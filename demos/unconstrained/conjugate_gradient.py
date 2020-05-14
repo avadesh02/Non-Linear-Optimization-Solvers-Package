ea82a33a2c2c7ec1b59525d4d57e26ef776ec7b1
avadesh02 <avadesh02@gmail.com>
2020-05-14 18:01:31 -0400

    updated CG demo

--- demos/unconstrained/conjugate_gradient.py
+++ demos/unconstrained/conjugate_gradient.py
@@ -5,19 +5,17 @@
 import torch
 from py_solvers.unconstrained.conjugate_gradient import FletcherReeves, PolakRibiere
 
-
-# def f(x):
-#     return torch.pow(2.0, x[0]*x[1]*x[0]) + 2
-
 def f(x):
-    return x[0]*x[0] + x[1]*x[1]
+    return x[0]*x[0]*x[1]*x[1] + (x[0] + x[1] + 2)**2
 
 fr = FletcherReeves(use_pure_fr = False)
-x0 = torch.tensor([[1000.0], [10.0]])
+x0 = torch.tensor([[-5.0], [2.0]])
 x_opt, f_opt = fr.optimize(f, x0, 100, 0.001, 10.0, use_btls = False)
+print(x_opt)
 fr.stats()
 
 pr = PolakRibiere(use_pure_pr = False)
-x0 = torch.tensor([[10.0], [10.0]])
+x0 = torch.tensor([[-5.0], [2.0]])
 x_opt, f_opt = pr.optimize(f, x0, 100, 0.001, 10.0, use_btls = False)
+print(x_opt)
 pr.stats()
