d2db15cb749b4d2f1a2ec48cdb0d8711f68c2a53
avadesh02 <avadesh02@gmail.com>
2020-05-14 19:20:08 -0400

    updated quasi newton demo

--- demos/unconstrained/quasi_newton.py
+++ demos/unconstrained/quasi_newton.py
@@ -5,15 +5,16 @@
 import torch
 from py_solvers.unconstrained.quasi_newton import BFGS
 
-bfgs = BFGS(1e-4, 0.9, 0.5)
-
-# def f(x):
-#     return torch.pow(2.0, x[0]*x[1]) + 2
 
 def f(x):
-    return x[0]*x[0]*x[1]*x[1]
+    return x[0]*x[0]*x[1]*x[1] + (x[0] + x[1] + 2)**2
 
-x0 = torch.tensor([[1.0], [2.0]])
+
+bfgs = BFGS(1e-4, 0.9, 0.5)
+
+x0 = torch.tensor([[-5.0], [2.0]])
 x_k, f_opt = bfgs.optimize(f, x0, 100, 0.001, use_btls = False, alpha_max = 2.0)
+print(x_k)
 bfgs.stats()
+
 
